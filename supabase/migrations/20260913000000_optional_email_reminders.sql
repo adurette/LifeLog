1de699eb93d8394ba8dec36cacc3a78abba95fe9
adurette <47675549+adurette@users.noreply.github.com>
2026-09-11 21:52:46 -0400

    Revert "Add optional email catch-up reminders"
    This reverts commit 97ba0b13a359a86b44307103356470a82a9fa6e1.

--- supabase/migrations/20260913000000_optional_email_reminders.sql
+++
@@ -1,4 +0,0 @@
-alter table public.profiles
-  add column email_reminders_enabled boolean not null default false,
-  add column email_reminder_time time not null default '20:00',
-  add column last_email_reminded_on date;
